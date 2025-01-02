47cb56357a8287fca6221ab1dc661c37cdfbe897
JYH <wjd6408@gmail.com>
2025-01-03 02:17:54 +0900

    임시Map 추가

--- ChuLoop/ChuLoop/Screens/MapScreen.swift
+++ ChuLoop/ChuLoop/Screens/MapScreen.swift
@@ -4,13 +4,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapScreen: View {
+    // 지도에서 사용할 위치 데이터
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // 줌 레벨
+    )
+    
     var body: some View {
         MainNavigationView(title: "맛집지도") {
-            VStack {
-                Text("지도 테스트")
-                    .padding()
+            ZStack {
+                // 지도 뷰가 화면 전체를 차지하도록 설정
+                Map(coordinateRegion: $region)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity) // 화면 크기에 맞게 확장
             }
         } onAddButtonTapped: {
             print("NavigationView Button Test")
@@ -18,6 +26,8 @@ struct MapScreen: View {
     }
 }
 
-#Preview {
-    MapScreen()
+struct MapScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MapScreen()
+    }
 }
