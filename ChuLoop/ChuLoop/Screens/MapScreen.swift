1a83b9a7890a84c68a45484d413003d6801035fa
JYH <wjd6408@gmail.com>
2025-01-03 13:52:47 +0900

    VisitScreen 임시 디자인 추가

--- ChuLoop/ChuLoop/Screens/MapScreen.swift
+++ ChuLoop/ChuLoop/Screens/MapScreen.swift
@@ -7,7 +7,7 @@ import SwiftUI
 import MapKit
 
 struct MapScreen: View {
-    // 지도에서 사용할 위치 데이터
+    // 지도에서 사용할 위치 데이터 (임시)
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // 줌 레벨
@@ -16,7 +16,6 @@ struct MapScreen: View {
     var body: some View {
         MainNavigationView(title: "맛집지도") {
             ZStack {
-                // 지도 뷰가 화면 전체를 차지하도록 설정
                 Map(coordinateRegion: $region)
                     .frame(maxWidth: .infinity, maxHeight: .infinity) // 화면 크기에 맞게 확장
             }
--- ChuLoop/ChuLoop/Screens/ShareScreen.swift
+++ ChuLoop/ChuLoop/Screens/ShareScreen.swift
@@ -8,7 +8,8 @@ import SwiftUI
 struct ShareScreen: View {
     var body: some View {
         MainNavigationView(title: "맛집 커뮤니티") {
-            Text("공유 화면 내용")
+            Text("공유된 맛집이 없어요\n타임라인에 올린 맛집을 함께 공유해보세요")
+                .multilineTextAlignment(.center)
         }
     }
 }
